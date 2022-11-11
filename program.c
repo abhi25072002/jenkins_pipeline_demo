c8959f9776f5c98a98518e48fccde63adf253660
Abhishek Dharmadhikari <abhishekdharmadhikari25@gmail.com>
2022-11-11 10:16:04 +0530

    change to be pushed

--- program.c
+++ program.c
@@ -4,5 +4,6 @@ int main(){
     int a = 344;
     printf("Addition of Two numbers is:%d\n",a+b);
     printf("Subtraction of Two numbers is:%d\n",a-b);
+    printf("Statement for triggering push");
     return 0;
 }
